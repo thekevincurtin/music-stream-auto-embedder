83861534edc673c5e0469d3b5e7f948321b28c6a
kevin <thekevincurtin@gmail.com>
2022-07-30 18:10:11 -0700

    Working well for RA and google, but need optimization for EDMTrain

--- hook.js
+++ hook.js
@@ -1,3 +1,8 @@
+// Main hook, listens for browsing activity then adds main listener 
+// without this hook, main.js will not always inject appropriately
+
+const updatedSet = new Set();
+
 chrome.tabs.onUpdated.addListener(function (tabId , info) {
     if (info.status === "complete") {
         chrome.scripting.executeScript({
--- main.js
+++ main.js
@@ -1,27 +1,45 @@
-const updatedSet = new Set();
+// Listens to any DOM updates and appends the OEmbed widget if necessary
+
+// const throws error due to page updates
+var SOUNDCLOUD_WIDGET_MAX_WIDTH = "300px";
+var INJECTION_DIV_MAP = {
+    "ra.co": "div .jBqnIi",
+    "edmtrain.com": "div .customSlider"
+}
+
+
 document.addEventListener("DOMNodeInserted", () => {
-    injectWidget();
+    createWidget();
 });
 
-function injectWidget() {
-    var links = document.querySelectorAll("a[href*='soundcloud.com']");
+function createWidget() {
+    console.log("Create widget");
+    const links = document.querySelectorAll("a[href*='soundcloud.com']");
     Array.from(links)
-        .filter((a) => !updatedSet.has(a.href))
-        .forEach((a) => {
-            let endpoint = "https://soundcloud.com/oembed?" + new URLSearchParams({
-                url: a.href,
-                maxwidth: "300px"
+        .filter((link) => !updatedSet.has(link.href))
+        .forEach((link) => {
+            const endpoint = "https://soundcloud.com/oembed?" + new URLSearchParams({
+                url: link.href,
+                maxwidth: SOUNDCLOUD_WIDGET_MAX_WIDTH
             })
-            updatedSet.add(a.href);
+            updatedSet.add(link.href);
             fetch(endpoint)
-                .then((r) => {
-                    return r.text();
+                .then((response) => {
+                    return response.text();
                 })
-                .then((t) => {
-                    let json = JSON.parse(t);
-                    let wrapper = document.createElement("div");
-                    wrapper.innerHTML = json.html;
-                    a.appendChild(wrapper);
+                .then((text) => {
+                    appendWidget(link, JSON.parse(text));
                 });
     });
+}
+
+function appendWidget(link, json) {
+    const wrapper = document.createElement("div");
+    wrapper.innerHTML = json.html;
+
+    const domain = window.location.hostname;
+    
+    const parentSelector = INJECTION_DIV_MAP[domain] ? INJECTION_DIV_MAP[domain] : "div";
+    link.closest(parentSelector).appendChild(wrapper);
+    console.log("appended");
 }
