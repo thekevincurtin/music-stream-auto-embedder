8afee5921aa5bade6d5717790c189351fe8893eb
kevin <thekevincurtin@gmail.com>
2022-08-10 18:57:54 -0700

    Initial commit for chrome store, soundcloud embed support for ra.co, edmtrain, google results

--- hook.js
+++ hook.js
@@ -2,17 +2,8 @@
 // without this hook, main.js logic will not always inject appropriately
 
 chrome.tabs.onUpdated.addListener(function (tabId , info) {
-    console.log("hook updated!");
     if (info.status === "complete") {
-        // console.log("executing main.js!");
-        // chrome.scripting.executeScript({
-        //     target: {tabId: tabId},
-        //     files: ["main.js"]
-        // });
-
-        //fires twice on edmtrain
         sendInitMessage(tabId);
-        console.log("sent init message");
     }
 });
 
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Listens to any DOM updates and appends the OEmbed widget if necessary
-console.log("main.js called");
+
 const SOUNDCLOUD_WIDGET_MAX_WIDTH = "300px";
 const INJECTION_DIV_MAP = {
     "ra.co": "div .jBqnIi",
@@ -9,44 +9,38 @@ var updatedSet;
 
 // Initialize
 chrome.runtime.onMessage.addListener((m) => {
-    console.log(m);
-
-    // Check for URLs on DOM updates
     if (document.getElementById("music-stream-auto-embedder-init") !== null) return;
     let statusDiv = document.createElement("div");
     statusDiv.setAttribute("id", "music-stream-auto-embedder-init");
     document.body.appendChild(statusDiv);
-    console.log("added status div");
 
-    // TODO fix refreshing on edmtrain
     updatedSet = new Set();
     
-    document.addEventListener("DOMNodeInserted", () => createWidget());
-    console.log("added listener");
+    // Check for valid URLs on DOM updates
+    document.addEventListener("DOMContentLoaded", () => createWidgets());
+    document.addEventListener("DOMNodeInserted", () => createWidgets());
 });
   
 
-function createWidget() {
+function createWidgets() {
     if (updatedSet == undefined) return;
-    const links = document.querySelectorAll("a[href*='soundcloud.com']");
+    const links = document.querySelectorAll("a[href*='soundcloud']");
     Array.from(links)
         .filter((link) => !updatedSet.has(link.href) && link.offsetParent !== null)
         .forEach((link) => {
-            console.log("start request");
             const endpoint = "https://soundcloud.com/oembed?" + new URLSearchParams({
                 url: link.href,
                 maxwidth: SOUNDCLOUD_WIDGET_MAX_WIDTH
-            })
+            });
             updatedSet.add(link.href);
             fetch(endpoint)
                 .then((response) => {
                     return response.text();
                 })
                 .then((text) => {
-                    console.log("append widget");
                     appendWidget(link, JSON.parse(text));
                 });
-    });
+        });
 }
 
 function appendWidget(link, json) {
@@ -56,6 +50,9 @@ function appendWidget(link, json) {
     const domain = window.location.hostname;
     
     const parentSelector = INJECTION_DIV_MAP[domain] ? INJECTION_DIV_MAP[domain] : "div";
-    link.closest(parentSelector).appendChild(wrapper);
-    console.log("appended");
+    const parentElement = link.closest(parentSelector) ? link.closest(parentSelector) : link.closest("div");
+    
+    if (parentElement != null) {
+        parentElement.appendChild(wrapper);
+    }
 }
